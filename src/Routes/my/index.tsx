35cebd287593718a266ecedf273113ce561e6f3b
luke7231 <tmddnjs7231@gmail.com>
2024-08-08 01:10:13 +0900

    feat: mypage에 link 추가

--- src/Routes/my/index.tsx
+++ src/Routes/my/index.tsx
@@ -1,11 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import BottomTab from "../../components/bottom-tab";
 
 const My = () => {
   return (
-    <div>
-      mypage~
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">My Page</h1>
+      <ul className="space-y-2">
+        <li>
+          <Link
+            to="/purchase-history"
+            className="text-blue-500 hover:underline"
+          >
+            Purchase History
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/customer-service"
+            className="text-blue-500 hover:underline"
+          >
+            Customer Service
+          </Link>
+        </li>
+        <li>
+          <Link to="/notice" className="text-blue-500 hover:underline">
+            Notice
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile-edit" className="text-blue-500 hover:underline">
+            Profile Edit
+          </Link>
+        </li>
+      </ul>
       <BottomTab />
     </div>
   );
 };
+
 export default My;
