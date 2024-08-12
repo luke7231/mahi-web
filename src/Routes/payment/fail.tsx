137329ec0b0ebe75135ca3f8328f711f958e7a1b
luke7231 <tmddnjs7231@gmail.com>
2024-08-12 23:12:29 +0900

    feat: fail page 작성

--- src/Routes/payment/fail.tsx
+++ src/Routes/payment/fail.tsx
@@ -1,14 +1,23 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function FailPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-
+  const message = searchParams.get("message") as string;
   // 고객에게 실패 사유 알려주고 다른 페이지로 이동
 
+  // 서버로 승인 요청
+  const onClick = () => {
+    // TODO: 원래 진행하고 있던 결제 상세페이지로 !
+    navigate("/payment");
+  };
   return (
     <div>
       <h1>결제 실패</h1>
       <div>{`사유: ${searchParams.get("message")}`}</div>
+      <div className="p-8" onClick={() => onClick()}>
+        돌아가기
+      </div>
     </div>
   );
 }
