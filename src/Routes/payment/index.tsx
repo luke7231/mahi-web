0130eeabf05c33d854dbfc42ab1ca7c70bb1e5d6
luke7231 <tmddnjs7231@gmail.com>
2024-08-12 23:08:27 +0900

    feat: 결제 성공 페이지 작성

--- src/Routes/payment/index.tsx
+++ src/Routes/payment/index.tsx
@@ -1,15 +1,35 @@
 import { useEffect, useRef } from "react";
 import {
   TossPaymentsSDK,
+  TossPaymentsWidgets,
   loadTossPayments,
 } from "@tosspayments/tosspayments-sdk";
+import { useMutation } from "@apollo/client";
+import { gql } from "../../__generated__";
+import { nanoid } from "nanoid";
 // import { ANONYMOUS } from "@tosspayments/payment-widget-sdk"
 
+// test 키
 const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
-
 const customerKey = "FF5zmAd8yIoiJ5fj4d5e2";
+const CREATE_ORDER = gql(`
+  mutation CreateOrder($input: CreateOrderInput!) {
+    createOrder(input: $input) {
+      id
+      orderId
+      amount
+      coupon
+      product {
+        id
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`);
 function Payment() {
-  const paymentWidgetRef = useRef<TossPaymentsSDK["widgets"] | null>(null);
+  const [createOrder] = useMutation(CREATE_ORDER);
+  const paymentWidgetRef = useRef<TossPaymentsWidgets | null>(null);
   const price = 50_000;
 
   useEffect(() => {
@@ -17,6 +37,12 @@ function Payment() {
       const paymentWidget = await (
         await loadTossPayments(clientKey)
       ).widgets({ customerKey });
+
+      // 가격 설정을 미리 해야하네?
+      await paymentWidget.setAmount({
+        currency: "KRW",
+        value: price,
+      });
 
       await Promise.all([
         // ------  결제 UI 렌더링 ------
@@ -31,13 +57,36 @@ function Payment() {
         }),
       ]);
 
-      // 가격 설정
-      await paymentWidget.setAmount({
-        currency: "KRW",
-        value: 50000,
-      });
+      paymentWidgetRef.current = paymentWidget;
     })();
   }, []);
+
+  const onClickPaymentButton = async () => {
+    const orderId = nanoid();
+    const { data } = await createOrder({
+      variables: {
+        input: {
+          orderId,
+          amount: price, // 이전 페이지에서 받아오기
+          productId: 8, // 이전 페이지에서 받아오기 or productID만 받아서 query 치기
+        },
+      },
+      onCompleted: (data) => console.log(data),
+    });
+    if (data?.createOrder.id) {
+      // request.
+      paymentWidgetRef.current?.requestPayment({
+        orderId,
+        orderName: "마감 A팩",
+        successUrl: window.location.origin + "/success",
+        failUrl: window.location.origin + "/fail",
+      });
+    } else {
+      // 문제가 발생했습니다.
+      // toast ui
+      console.log("문제 발생했습니다.");
+    }
+  };
 
   return (
     <div className="App">
@@ -47,7 +96,11 @@ function Payment() {
       {/* <!-- 이용약관 UI --> */}
       <div id="agreement"></div>
       {/* <!-- 결제하기 버튼 --> */}
-      <button id="payment-button" className="mt-[30px]">
+      <button
+        id="payment-button"
+        className="mt-[30px]"
+        onClick={() => onClickPaymentButton()}
+      >
         결제하기
       </button>
     </div>
--- src/Routes/payment/success.tsx
+++ src/Routes/payment/success.tsx
@@ -1,17 +1,78 @@
-import { useSearchParams } from "react-router-dom";
+// import { gql, useQuery } from "@apollo/client";
+
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { gql } from "../../__generated__";
+import { useQuery } from "@apollo/client";
+import { useState } from "react";
+const CHECK_AMOUNT = gql(`
+  query Query($orderId: String!, $amount: Float!, $paymentKey: String!) {
+    compareOrderAmount(
+      orderId: $orderId
+      amount: $amount
+      paymentKey: $paymentKey
+    ) {
+      ok
+      error
+    }
+  }
+`);
 
 export function SuccessPage() {
+  const navigate = useNavigate();
+  const [승인완료, set승인완료] = useState(false);
+  const [에러발생, set에러발생] = useState(false);
+  const [에러메세지, set에러메세지] = useState("");
   const [searchParams] = useSearchParams();
+  const orderId = searchParams.get("orderId") as string;
+  const amount = searchParams.get("amount") as string;
+  const paymentKey = searchParams.get("paymentKey") as string;
+  const { data } = useQuery(CHECK_AMOUNT, {
+    variables: { orderId, amount: Number(amount), paymentKey },
+    onCompleted: (data) => {
+      console.log(data);
+      if (data.compareOrderAmount.ok) {
+        set승인완료(true);
+      } else {
+        // 문제발생했습니다! 알려주고, 결제페이지로 다시 보냄.
+        set에러발생(true);
+        set에러메세지(data.compareOrderAmount.error as string);
+      }
+    },
+  });
 
   // 서버로 승인 요청
-
+  const onClick = () => {
+    if (에러발생) {
+      navigate(`/payment`);
+    }
+    if (승인완료) {
+      navigate("/");
+    }
+  };
   return (
     <div>
-      <h1>결제 성공</h1>
-      <div>{`주문 아이디: ${searchParams.get("orderId")}`}</div>
-      <div>{`결제 금액: ${Number(
-        searchParams.get("amount")
-      ).toLocaleString()}원`}</div>
+      {승인완료 ? (
+        <>
+          <h1>결제 성공</h1>
+          <div>{`주문 아이디: ${searchParams.get("orderId")}`}</div>
+          <div>{`결제 금액: ${Number(
+            searchParams.get("amount")
+          ).toLocaleString()}원`}</div>
+          <div className="p-8" onClick={() => onClick()}>
+            완료
+          </div>
+        </>
+      ) : null}
+      {에러발생 ? (
+        <>
+          <h1>결제 실패!</h1>
+          <div>결제에 실패하였습니다. 다시 시도해주세요.</div>
+          <div>사유: {에러메세지}</div>
+          <div className="p-8" onClick={() => onClick()}>
+            돌아가기
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }
