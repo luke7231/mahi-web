dc0872a2a4f13b67637d8fe6c307d22aff409ec9
luke7231 <tmddnjs7231@gmail.com>
2024-08-20 22:50:03 +0900

    feat: user(계정) 삭제 기능 추가

--- src/Routes/my/index.tsx
+++ src/Routes/my/index.tsx
@@ -1,8 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import BottomTab from "../../components/bottom-tab";
+import { gql, useMutation } from "@apollo/client";
+import { useAuth } from "../../core/auth";
 
+const DELETE_USER = gql`
+  mutation KakaoDeleteUser {
+    kakaoDeleteUser {
+      ok
+      error
+    }
+  }
+`;
 const My = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  const [deleteUser, { error }] = useMutation(DELETE_USER);
+  function onClickDelete() {
+    deleteUser({
+      onCompleted: (data) => {
+        if (data.kakaoDeleteUser.ok) {
+          localStorage.removeItem("jwt");
+          logout();
+          navigate("/");
+        }
+      },
+    });
+  }
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">My Page</h1>
@@ -32,6 +56,14 @@ const My = () => {
           <Link to="/profile-edit" className="text-blue-500 hover:underline">
             Profile Edit
           </Link>
+        </li>
+        <li>
+          <div
+            className="text-blue-500 hover:underline"
+            onClick={() => onClickDelete()}
+          >
+            Delete Account
+          </div>
         </li>
       </ul>
       <BottomTab />
