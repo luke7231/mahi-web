075d1cb5ba6e11377017b3b751658aa888b283d6
luke7231 <tmddnjs7231@gmail.com>
2024-08-22 01:59:47 +0900

    feat: codegen 출원지 변경

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,8 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000",
-  // schema: "https://mahi-server.onrender.com",
+  // schema: "http://localhost:4000",
+  schema: "https://mahi-server.onrender.com",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ["src/**/*.{ts,tsx}"],
   generates: {
