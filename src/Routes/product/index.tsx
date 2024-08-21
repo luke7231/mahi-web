a933e41242e22a2c746ee157118cbdee9845c39a
luke7231 <tmddnjs7231@gmail.com>
2024-08-22 01:35:09 +0900

    feat: product page -> 홈페이지 하단 사업자 정보 추가

--- src/Routes/product/index.tsx
+++ src/Routes/product/index.tsx
@@ -146,6 +146,34 @@ const Product = () => {
           </div>
         </>
       ) : null}
+      {/* Business Information */}
+      <div className="bg-gray-100 p-6 mt-8">
+        <h2 className="text-md font-semibold text-gray-800 mb-4">
+          사업자 정보
+        </h2>
+        <div className="text-gray-700 text-xs">
+          <p className="mb-2">
+            <span className="font-bold">상호명: </span>
+            육각형
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">대표자명: </span>
+            원요한
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">사업자등록번호: </span>
+            329-49-00934
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">사업장 주소: </span>
+            경기도 성남시 수정구 성남대로 1342, 6층 617호 (복정동)
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">유선번호: </span>
+            010-9073-0701
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
