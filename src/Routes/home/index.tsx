4458c398f46abf7ece2bba13a46d6c46914300ca
luke7231 <tmddnjs7231@gmail.com>
2024-08-25 00:56:29 +0900

    feat: 위치 설정, nostore comoponent 적용

--- src/Routes/home/index.tsx
+++ src/Routes/home/index.tsx
@@ -11,6 +11,7 @@ import { client } from "../../App";
 import { Store } from "../../__generated__/graphql";
 import BottomTab from "../../components/bottom-tab";
 import { StoreCard } from "../../components/store_card";
+import { NoStore } from "../../components/home/no-store";
 // localStorage.clear();
 const GET_STORES = gql(`
   query Stores($lat: Float, $lng: Float) {
@@ -139,11 +140,31 @@ const Home = () => {
     <div className="w-full h-[100vh] flex flex-col bg-white">
       {/* LOCATION */}
       <div
-        className="p-2 bg-gray-100 flex justify-center items-center cursor-pointer "
+        className="py-4 pl-5 bg-white flex items-center cursor-pointer "
         onClick={() => navigate("/location")}
       >
-        <span className="text-blue-500 font-bold underline">
-          📍 내 위치를 설정해주세요.
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="21"
+          viewBox="0 0 21 21"
+          fill="none"
+        >
+          <path
+            d="M15.4499 14.5749L11.7373 18.2875C11.5749 18.45 11.3821 18.5789 11.1699 18.6669C10.9576 18.7549 10.7302 18.8002 10.5004 18.8002C10.2707 18.8002 10.0432 18.7549 9.83101 18.6669C9.61879 18.5789 9.42599 18.45 9.26363 18.2875L5.55013 14.5749C4.5712 13.5959 3.90455 12.3486 3.63448 10.9907C3.36441 9.63289 3.50306 8.22544 4.03288 6.94638C4.5627 5.66732 5.45991 4.57409 6.61105 3.80493C7.76219 3.03578 9.11555 2.62524 10.5 2.62524C11.8845 2.62524 13.2378 3.03578 14.389 3.80493C15.5401 4.57409 16.4373 5.66732 16.9671 6.94638C17.4969 8.22544 17.6356 9.63289 17.3655 10.9907C17.0955 12.3486 16.4288 13.5959 15.4499 14.5749Z"
+            stroke="black"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M13.125 9.625C13.125 10.3212 12.8484 10.9889 12.3562 11.4812C11.8639 11.9734 11.1962 12.25 10.5 12.25C9.80381 12.25 9.13613 11.9734 8.64384 11.4812C8.15156 10.9889 7.875 10.3212 7.875 9.625C7.875 8.92881 8.15156 8.26113 8.64384 7.76884C9.13613 7.27656 9.80381 7 10.5 7C11.1962 7 11.8639 7.27656 12.3562 7.76884C12.8484 8.26113 13.125 8.92881 13.125 9.625Z"
+            stroke="black"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <span className="ml-1 text-black font-semibold text-lg">
+          내 위치를 설정해주세요.
         </span>
       </div>
 
@@ -181,12 +202,13 @@ const Home = () => {
       </div>
       <div className="h-[0.0625rem] w-full bg-[#F4F5F7]" />
       {/* LIST */}
+      {isList && loading ? (
+        <div className="text-center text-gray-500">Loading...</div>
+      ) : null}
       {isList ? (
-        <div className="mt-8 p-4 flex-1 overflow-y-auto">
-          {loading ? (
-            <div className="text-center text-gray-500">Loading...</div>
-          ) : (
-            data?.stores?.map((store) => (
+        data?.stores?.length !== 0 ? (
+          <div className="mt-8 p-4 flex-1 overflow-y-auto">
+            {data?.stores?.map((store) => (
               <div
                 key={store?.id}
                 className="h-[120px] shadow-md mb-4 flex items-center p-4 bg-gray-50 rounded-md cursor-pointer hover:shadow-lg transition-shadow"
@@ -210,14 +232,14 @@ const Home = () => {
                   {store?.isLiked ? "❤️" : "🤍"}
                 </div>
               </div>
-            ))
-          )}
-          <StoreCard />
-        </div>
-      ) : (
-        <Map stores={data?.stores as Store[]} />
-      )}
-
+            ))}
+            <StoreCard />
+          </div>
+        ) : (
+          <NoStore />
+        )
+      ) : null}
+      {/* {data?.stores?.length !== 0 ? <NoStore /> : null} */}
       {/* BOTTOM TAB */}
       <BottomTab />
     </div>
