b45b2612e5e2fd1fc786a02f650cc589e81d7da2
luke7231 <tmddnjs7231@gmail.com>
2024-08-26 19:32:53 +0900

    feat: onboarding 페이지 디자인 적용

--- src/Routes/onboarding/onboarding1.tsx
+++ src/Routes/onboarding/onboarding1.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { postMessage } from "../../core/message";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../core/auth";
-
+import IMG from "./location.png";
 const Onboarding1 = () => {
   const navigate = useNavigate();
   const { doneOnboarding } = useAuth();
@@ -39,16 +39,42 @@ const Onboarding1 = () => {
   return (
     <div className="h-[100vh]">
       <div className="h-full flex flex-col items-center justify-between">
-        <div></div>
-        <div>
-          <p className="font-bold text-center mb-4 text-xl">1. 위치 설정</p>
-          <p>내 근처 마감 상품을 볼 수 있어요!</p>
+        <div className="w-full max-w-xs h-full flex flex-col justify-center items-center p-4">
+          {/* Image */}
+          <img
+            className="w-full max-w-xs h-auto"
+            src={IMG}
+            alt="Location Illustration"
+          />
+
+          {/* Text Section */}
+          <div className="text-center">
+            <div className="text-black text-2xl font-semibold mb-5">
+              위치 설정이 필요해요
+            </div>
+            <div className="text-black text-sm font-normal">
+              위치 설정에 동의하시면
+              <br />내 근처 마감 상품을 볼 수 있어요!
+            </div>
+          </div>
         </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="33"
+          height="7"
+          viewBox="0 0 33 7"
+          fill="none"
+          className="mb-7"
+        >
+          <circle cx="3.5" cy="3.5" r="3.5" fill="#1562FC" />
+          <circle cx="16.5" cy="3.5" r="3.5" fill="#D9D9D9" />
+          <circle cx="29.5" cy="3.5" r="3.5" fill="#D9D9D9" />
+        </svg>
         <button
-          className="text-white font-bold p-3 bg-gray-800 w-full"
+          className="text-white font-semibold h-[60px] bg-[#1562fc] w-full"
           onClick={onClickButton}
         >
-          위치 설정
+          다음
         </button>
       </div>
     </div>
