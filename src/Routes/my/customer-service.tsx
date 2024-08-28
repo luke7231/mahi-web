716b9dc9b7040eb4760982bd1bbcacb61f4d8cbf
luke7231 <tmddnjs7231@gmail.com>
2024-08-28 14:54:46 +0900

    feat: 약관 및 정책 ui 설정

--- src/Routes/my/customer-service.tsx
+++ src/Routes/my/customer-service.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Menu from "../../components/my/menu";
+import BottomTab from "../../components/bottom-tab";
 
 const CustomerService = () => {
   return (
-    <div>
-      <h1>Customer Service</h1>
-      {/* Your content goes here */}
+    <div className="w-full h-[100vh] flex flex-col">
+      {/* 헤더 */}
+      <div className="flex justify-center py-4 text-lg font-semibold">
+        약관 및 정책
+      </div>
+      <div className="h-[0.0625rem] w-full bg-[#eaeaea]" />
+
+      {/* 메뉴   */}
+      <Menu
+        title="개인정보 처리방침"
+        to="https://quirky-moss-44e.notion.site/4fae5a1a1faa4f6fb569e5ec4f07e9c5?pvs=4"
+      />
+      <Menu title="서비스 이용 약관" to="" />
+
+      <BottomTab />
     </div>
   );
 };
