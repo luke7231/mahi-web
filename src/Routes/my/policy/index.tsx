68f94060cb47f28fa7a967de189b4755e179e3a0
luke7231 <tmddnjs7231@gmail.com>
2024-08-28 14:56:03 +0900

    사실 약관은 policy 였던거

--- src/Routes/my/policy/index.tsx
+++ src/Routes/my/policy/index.tsx
@@ -1,24 +1,23 @@
 import { Link, useNavigate } from "react-router-dom";
 import BottomTab from "../../../components/bottom-tab";
+import Menu from "../../../components/my/menu";
 
 const Policy = () => {
   return (
-    <div>
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">약관 및 정책</h1>
-        <ul className="space-y-2">
-          <li>
-            <Link to="/" className="text-blue-500 hover:underline">
-              개인정보 처리 방침
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="text-blue-500 hover:underline">
-              서비스 이용 약관
-            </Link>
-          </li>
-        </ul>
+    <div className="w-full h-[100vh] flex flex-col">
+      {/* 헤더 */}
+      <div className="flex justify-center py-4 text-lg font-semibold">
+        약관 및 정책
       </div>
+      <div className="h-[0.0625rem] w-full bg-[#eaeaea]" />
+
+      {/* 메뉴   */}
+      <Menu
+        title="개인정보 처리방침"
+        to="https://quirky-moss-44e.notion.site/4fae5a1a1faa4f6fb569e5ec4f07e9c5?pvs=4"
+      />
+      <Menu title="서비스 이용 약관" to="" />
+
       <BottomTab />
     </div>
   );
