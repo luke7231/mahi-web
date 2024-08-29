89204c18676fd5abca47a9ef11fcb13ce3bb589b
luke7231 <tmddnjs7231@gmail.com>
2024-08-29 17:10:46 +0900

    feat: cart button 생성 (at Home)

--- src/Routes/home/index.tsx
+++ src/Routes/home/index.tsx
@@ -7,11 +7,12 @@ import { useLocation } from "../../core/location-provider";
 import { gql } from "../../__generated__";
 import { gql as apolloGql } from "@apollo/client";
 import { useAuth } from "../../core/auth";
-import { client } from "../../App";
 import { Product, Store } from "../../__generated__/graphql";
 import BottomTab from "../../components/bottom-tab";
 import { StoreCard } from "../../components/store_card";
 import { NoStore } from "../../components/home/no-store";
+import { postMessage } from "../../core/message";
+import { useCart } from "../../core/cart";
 // localStorage.clear();
 const GET_STORES = gql(`
   query Stores($lat: Float, $lng: Float) {
@@ -59,6 +60,7 @@ export const CANCEL_LIKE = apolloGql`
   }
 `;
 const Home = () => {
+  const { cart } = useCart();
   const { isLoggedIn } = useAuth();
   const [isList, setIsList] = useState(true);
   const { hasLastLo, getLocationFromStorage } = useLocation();
@@ -138,36 +140,66 @@ const Home = () => {
   function onClickStore(id: number) {
     navigate(`store/${id}`);
   }
+  function onClickCart() {
+    if (!isLoggedIn) {
+      navigate("/login");
+      return;
+    }
+    navigate("/checkout");
+  }
   return (
     <div className="w-full h-[100vh] flex flex-col bg-white">
       {/* LOCATION */}
-      <div
-        className="py-3 pl-5 bg-white flex items-center cursor-pointer "
-        onClick={() => navigate("/location")}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="21"
-          height="21"
-          viewBox="0 0 21 21"
-          fill="none"
-        >
-          <path
-            d="M15.4499 14.5749L11.7373 18.2875C11.5749 18.45 11.3821 18.5789 11.1699 18.6669C10.9576 18.7549 10.7302 18.8002 10.5004 18.8002C10.2707 18.8002 10.0432 18.7549 9.83101 18.6669C9.61879 18.5789 9.42599 18.45 9.26363 18.2875L5.55013 14.5749C4.5712 13.5959 3.90455 12.3486 3.63448 10.9907C3.36441 9.63289 3.50306 8.22544 4.03288 6.94638C4.5627 5.66732 5.45991 4.57409 6.61105 3.80493C7.76219 3.03578 9.11555 2.62524 10.5 2.62524C11.8845 2.62524 13.2378 3.03578 14.389 3.80493C15.5401 4.57409 16.4373 5.66732 16.9671 6.94638C17.4969 8.22544 17.6356 9.63289 17.3655 10.9907C17.0955 12.3486 16.4288 13.5959 15.4499 14.5749Z"
-            stroke="black"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M13.125 9.625C13.125 10.3212 12.8484 10.9889 12.3562 11.4812C11.8639 11.9734 11.1962 12.25 10.5 12.25C9.80381 12.25 9.13613 11.9734 8.64384 11.4812C8.15156 10.9889 7.875 10.3212 7.875 9.625C7.875 8.92881 8.15156 8.26113 8.64384 7.76884C9.13613 7.27656 9.80381 7 10.5 7C11.1962 7 11.8639 7.27656 12.3562 7.76884C12.8484 8.26113 13.125 8.92881 13.125 9.625Z"
-            stroke="black"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <span className="ml-1 text-black font-semibold text-md">
-          내 위치를 설정해주세요.
-        </span>
+      <div className="py-3 pl-5 bg-white flex items-center cursor-pointer justify-between">
+        <div className="flex" onClick={() => navigate("/location")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21"
+            fill="none"
+          >
+            <path
+              d="M15.4499 14.5749L11.7373 18.2875C11.5749 18.45 11.3821 18.5789 11.1699 18.6669C10.9576 18.7549 10.7302 18.8002 10.5004 18.8002C10.2707 18.8002 10.0432 18.7549 9.83101 18.6669C9.61879 18.5789 9.42599 18.45 9.26363 18.2875L5.55013 14.5749C4.5712 13.5959 3.90455 12.3486 3.63448 10.9907C3.36441 9.63289 3.50306 8.22544 4.03288 6.94638C4.5627 5.66732 5.45991 4.57409 6.61105 3.80493C7.76219 3.03578 9.11555 2.62524 10.5 2.62524C11.8845 2.62524 13.2378 3.03578 14.389 3.80493C15.5401 4.57409 16.4373 5.66732 16.9671 6.94638C17.4969 8.22544 17.6356 9.63289 17.3655 10.9907C17.0955 12.3486 16.4288 13.5959 15.4499 14.5749Z"
+              stroke="black"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M13.125 9.625C13.125 10.3212 12.8484 10.9889 12.3562 11.4812C11.8639 11.9734 11.1962 12.25 10.5 12.25C9.80381 12.25 9.13613 11.9734 8.64384 11.4812C8.15156 10.9889 7.875 10.3212 7.875 9.625C7.875 8.92881 8.15156 8.26113 8.64384 7.76884C9.13613 7.27656 9.80381 7 10.5 7C11.1962 7 11.8639 7.27656 12.3562 7.76884C12.8484 8.26113 13.125 8.92881 13.125 9.625Z"
+              stroke="black"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <span className="ml-1 text-black font-semibold text-md">
+            내 위치를 설정해주세요.
+          </span>
+        </div>
+        <div className="relative inline-block mr-5 mt-1" onClick={onClickCart}>
+          {/* Cart Icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M6 6h15l-1.5 9h-13L6 6z"></path>
+            <circle cx="9" cy="20" r="1"></circle>
+            <circle cx="18" cy="20" r="1"></circle>
+            <path d="M6 6L4 2"></path>
+          </svg>
+
+          {/* Number Badge */}
+          <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-red-500 flex items-center justify-center text-white text-xs font-bold">
+            {cart.length}
+          </div>
+        </div>
       </div>
 
       {/* <div className="p-2 text-sm text-gray-600">
