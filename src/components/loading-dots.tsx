111c907b638be0c8afb79c60ace5c27ca310716a
luke7231 <tmddnjs7231@gmail.com>
2024-09-05 13:18:11 +0900

    feat: loading dots 컴포넌트 생성

---
+++ src/components/loading-dots.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoadingDots = () => {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50">
+      <div className="flex space-x-2">
+        <div className="w-3 h-3 bg-[#1562fc] rounded-full animate-bounce-sequence"></div>
+        <div className="w-3 h-3 bg-[#1562fc] rounded-full animate-bounce-sequence animation-delay-200"></div>
+        <div className="w-3 h-3 bg-[#1562fc] rounded-full animate-bounce-sequence animation-delay-400"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingDots;
