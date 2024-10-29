645d3d3b584b962aa57c419aeaee2dfa692bb126
luke7231 <tmddnjs7231@gmail.com>
2024-10-29 17:51:51 +0900

    feat: product -> skeleton ui

--- src/Routes/product/index.tsx
+++ src/Routes/product/index.tsx
@@ -5,7 +5,9 @@ import { useCart } from "../../core/cart";
 import { useAuth } from "../../core/auth";
 import Partition from "../../components/common/partition";
 import BACK_IMG from "../../components/common/back-img.png";
-import LoadingDots from "../../components/loading-dots";
+
+import Skeleton from "react-loading-skeleton";
+
 const GET_PRODUCT = gql`
   query Product($productId: Int!) {
     product(id: $productId) {
@@ -79,25 +81,29 @@ const Product = () => {
       setQuantity(0);
     }
   }, [product?.quantity]);
-  console.log(product);
+
   return (
     <div className="container h-[100vh] mx-auto bg-[#F4F5F7]">
-      {loading ? <LoadingDots /> : null}
-      {product ? (
-        <>
-          <div className="relative w-full mx-auto bg-white overflow-hidden">
-            {/* Image Container */}
-            <div className="relative w-full">
-              <img
-                src={BACK_IMG}
-                onClick={() => navigate(-1)}
-                className="absolute top-5 left-5 w-[40px] h-[40px]"
-              />
+      <>
+        <div className="relative w-full mx-auto bg-white overflow-hidden">
+          {/* Image Container */}
+          <div className="relative w-full">
+            <img
+              src={BACK_IMG}
+              onClick={() => navigate(-1)}
+              className="absolute top-5 left-5 w-[40px] h-[40px]"
+            />
 
-              <div className="flex overflow-scroll">
-                {product.menus ? (
-                  <>
-                    {product.menus.map((menu: { id: number; img: string }) => (
+            <div className="flex overflow-scroll">
+              {loading && (
+                <Skeleton
+                  style={{ height: 240, width: "100vw", lineHeight: 2 }}
+                />
+              )}
+              {product?.menus ? (
+                <>
+                  {!loading &&
+                    product.menus.map((menu: { id: number; img: string }) => (
                       <div className="relative">
                         <img src={menu.img} className="h-60 aspect-auto" />
                         <div className="absolute bottom-2 right-1.5 rounded-md opacity-80 bg-black text-white p-2 px-2.5">
@@ -107,123 +113,143 @@ const Product = () => {
                         </div>
                       </div>
                     ))}
-                  </>
-                ) : (
-                  <img
-                    className="object-cover w-full h-56"
-                    alt="Product"
-                    src={(product.img as string) || product.menus[0].img}
-                  />
-                )}
-              </div>
-            </div>
-
-            {/* Store Name */}
-            <div className="px-4 py-3">
-              <div className="flex justify-between mb-3 ">
-                <h2 className="text-3xl font-semibold text-black">
-                  {product.name}
-                </h2>
-                {/* Favorite Icon */}
-                <div className="flex items-center"></div>
-              </div>
-
-              {/* Pickup and Walk Time Info */}
-              <div className="flex items-center text-md">
-                {product.description}
-              </div>
+                </>
+              ) : (
+                <img
+                  className="object-cover w-full h-56"
+                  alt="Product"
+                  src={(product?.img as string) || product?.menus[0].img}
+                />
+              )}
             </div>
           </div>
-          <Partition color="light" height="thick" />
-          <div className="w-full p-5 h-auto flex justify-between items-center bg-white">
-            {/* Price Label */}
-            <div className="text-black text-lg font-semibold">가격</div>
 
-            {/* Prices */}
-            <div className="flex items-center space-x-2">
-              {/* Original Price */}
+          {/* Store Name */}
+          <div className="px-4 py-3">
+            <div className="flex justify-between mb-3 ">
+              {loading ? (
+                <Skeleton style={{ height: 31, width: 80 }} />
+              ) : (
+                <h2 className="text-3xl font-semibold text-black">
+                  {product?.name}
+                </h2>
+              )}
+              {/* Favorite Icon */}
+              <div className="flex items-center"></div>
+            </div>
+
+            {/* Pickup and Walk Time Info */}
+            <div className="flex items-center text-md">
+              {product?.description}
+            </div>
+          </div>
+        </div>
+        <Partition color="light" height="thick" />
+        <div className="w-full p-5 h-auto flex justify-between items-center bg-white">
+          {/* Price Label */}
+          {loading ? (
+            <Skeleton style={{ height: 20, width: 58 }} />
+          ) : (
+            <div className="text-black text-lg font-semibold">가격</div>
+          )}
+
+          {/* Prices */}
+          <div className="flex items-center space-x-2">
+            {/* Original Price */}
+            {loading ? (
+              <Skeleton style={{ height: 20, width: 58 }} />
+            ) : (
               <div className="text-[#dadada] text-lg line-through">
                 {product?.price.toLocaleString()}원
               </div>
+            )}
 
-              {/* Discounted Price */}
+            {/* Discounted Price */}
+            {loading ? (
+              <Skeleton style={{ height: 20, width: 58 }} />
+            ) : (
               <div className="text-black text-lg font-bold">
-                {product.userPrice.toLocaleString()}원
+                {product?.userPrice.toLocaleString()}원
               </div>
-            </div>
+            )}
           </div>
-          <Partition color="light" height="md" />
-          <div className="w-full h-auto flex items-center justify-between px-5 py-5 bg-white">
-            {/* Label */}
-            <div className="flex items-center">
+        </div>
+        <Partition color="light" height="md" />
+        <div className="w-full h-auto flex items-center justify-between px-5 py-5 bg-white">
+          {/* Label */}
+          <div className="flex items-center">
+            {loading ? (
+              <Skeleton style={{ height: 20, width: 58 }} />
+            ) : (
               <div className="text-black text-lg font-semibold">수량</div>
-              <span className="text-xs text-[#1562fc] ml-2">
-                {product.quantity}개 남음
+            )}
+
+            <span className="text-xs text-[#1562fc] ml-2">
+              {product?.quantity}개 남음
+            </span>
+          </div>
+          {/* Quantity Controls */}
+          <div className="flex items-center">
+            {/* Minus Button */}
+            <div
+              onClick={decreaseQuantity}
+              className="w-[43px] h-[43px] flex items-center justify-center border border-[#e1e1e1] rounded-tl-[3px] rounded-bl-[3px]"
+            >
+              <span className="text-black text-xl">-</span>
+            </div>
+
+            {/* Quantity Display with Only Top and Bottom Borders */}
+            <div className="w-[43px] h-[43px] flex items-center justify-center border-t border-b border-[#e1e1e1]">
+              <span className="text-black text-base font-semibold">
+                {quantity}
               </span>
             </div>
-            {/* Quantity Controls */}
-            <div className="flex items-center">
-              {/* Minus Button */}
-              <div
-                onClick={decreaseQuantity}
-                className="w-[43px] h-[43px] flex items-center justify-center border border-[#e1e1e1] rounded-tl-[3px] rounded-bl-[3px]"
-              >
-                <span className="text-black text-xl">-</span>
-              </div>
 
-              {/* Quantity Display with Only Top and Bottom Borders */}
-              <div className="w-[43px] h-[43px] flex items-center justify-center border-t border-b border-[#e1e1e1]">
-                <span className="text-black text-base font-semibold">
-                  {quantity}
-                </span>
-              </div>
-
-              {/* Plus Button */}
-              <div
-                onClick={increaseQuantity}
-                className="w-[43px] h-[43px] flex items-center justify-center border border-[#e1e1e1] rounded-tr-[3px] rounded-br-[3px]"
-              >
-                <span className="text-black text-xl">+</span>
-              </div>
+            {/* Plus Button */}
+            <div
+              onClick={increaseQuantity}
+              className="w-[43px] h-[43px] flex items-center justify-center border border-[#e1e1e1] rounded-tr-[3px] rounded-br-[3px]"
+            >
+              <span className="text-black text-xl">+</span>
             </div>
           </div>
-          <Partition color="light" height="md" />
+        </div>
+        <Partition color="light" height="md" />
 
-          {/* Business Information */}
-          <div className={`w-full h-80 bg-[#F4F5F7]`} />
-          <div className="bg-[#F4F5F7] p-6 pb-20">
-            <h2 className="text-md font-semibold text-gray-800 mb-4">
-              사업자 정보
-            </h2>
-            <div className="text-gray-700 text-xs">
-              <p className="mb-2">
-                <span className="font-bold">상호명: </span>
-                육각형
-              </p>
-              <p className="mb-2">
-                <span className="font-bold">대표자명: </span>
-                원요한
-              </p>
-              <p className="mb-2">
-                <span className="font-bold">사업자등록번호: </span>
-                329-49-00934
-              </p>
-              <p className="mb-2">
-                <span className="font-bold">통신판매신고: </span>
-                2024-성남수정-0892호
-              </p>
-              <p className="mb-2">
-                <span className="font-bold">사업장 주소: </span>
-                경기도 성남시 수정구 성남대로 1342, 6층 617호 (복정동)
-              </p>
-              <p className="mb-2">
-                <span className="font-bold">유선번호: </span>
-                010-9073-0701
-              </p>
-            </div>
+        {/* Business Information */}
+        <div className={`w-full h-80 bg-[#F4F5F7]`} />
+        <div className="bg-[#F4F5F7] p-6 pb-20">
+          <h2 className="text-md font-semibold text-gray-800 mb-4">
+            사업자 정보
+          </h2>
+          <div className="text-gray-700 text-xs">
+            <p className="mb-2">
+              <span className="font-bold">상호명: </span>
+              육각형
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">대표자명: </span>
+              원요한
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">사업자등록번호: </span>
+              329-49-00934
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">통신판매신고: </span>
+              2024-성남수정-0892호
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">사업장 주소: </span>
+              경기도 성남시 수정구 성남대로 1342, 6층 617호 (복정동)
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">유선번호: </span>
+              010-9073-0701
+            </p>
           </div>
-        </>
-      ) : null}
+        </div>
+      </>
       {product?.quantity !== 0 ? (
         <div
           onClick={() => onClickButton()}
