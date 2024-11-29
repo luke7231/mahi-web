095deb39915ea260164cc5c8e401d042befd125a
luke7231 <tmddnjs7231@gmail.com>
2024-11-29 20:31:02 +0900

    feat: business info 컴포넌트 생성

---
+++ src/Routes/product/business-info.tsx
@@ -0,0 +1,34 @@
+const BusinessInfo = () => {
+  return (
+    <div className="bg-[#F4F5F7] p-6 pb-20">
+      <h2 className="text-md font-semibold text-gray-800 mb-4">사업자 정보</h2>
+      <div className="text-gray-700 text-xs">
+        <p className="mb-2">
+          <span className="font-bold">상호명: </span>
+          육각형
+        </p>
+        <p className="mb-2">
+          <span className="font-bold">대표자명: </span>
+          원요한
+        </p>
+        <p className="mb-2">
+          <span className="font-bold">사업자등록번호: </span>
+          329-49-00934
+        </p>
+        <p className="mb-2">
+          <span className="font-bold">통신판매신고: </span>
+          2024-성남수정-0892호
+        </p>
+        <p className="mb-2">
+          <span className="font-bold">사업장 주소: </span>
+          경기도 성남시 수정구 성남대로 1342, 6층 617호 (복정동)
+        </p>
+        <p className="mb-2">
+          <span className="font-bold">유선번호: </span>
+          010-9073-0701
+        </p>
+      </div>
+    </div>
+  );
+};
+export default BusinessInfo;
--- src/Routes/product/index.tsx
+++ src/Routes/product/index.tsx
@@ -7,6 +7,7 @@ import Partition from "../../components/common/partition";
 import BACK_IMG from "../../components/common/back-img.png";
 
 import Skeleton from "react-loading-skeleton";
+import BusinessInfo from "./business-info";
 
 const GET_PRODUCT = gql`
   query Product($productId: Int!) {
@@ -218,37 +219,7 @@ const Product = () => {
 
         {/* Business Information */}
         <div className={`w-full h-80 bg-[#F4F5F7]`} />
-        <div className="bg-[#F4F5F7] p-6 pb-20">
-          <h2 className="text-md font-semibold text-gray-800 mb-4">
-            사업자 정보
-          </h2>
-          <div className="text-gray-700 text-xs">
-            <p className="mb-2">
-              <span className="font-bold">상호명: </span>
-              육각형
-            </p>
-            <p className="mb-2">
-              <span className="font-bold">대표자명: </span>
-              원요한
-            </p>
-            <p className="mb-2">
-              <span className="font-bold">사업자등록번호: </span>
-              329-49-00934
-            </p>
-            <p className="mb-2">
-              <span className="font-bold">통신판매신고: </span>
-              2024-성남수정-0892호
-            </p>
-            <p className="mb-2">
-              <span className="font-bold">사업장 주소: </span>
-              경기도 성남시 수정구 성남대로 1342, 6층 617호 (복정동)
-            </p>
-            <p className="mb-2">
-              <span className="font-bold">유선번호: </span>
-              010-9073-0701
-            </p>
-          </div>
-        </div>
+        <BusinessInfo />
       </>
       {product?.quantity !== 0 ? (
         <div
