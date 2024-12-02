9dafdb7d5cf56cbe95e794995d5b9d7950d0d0dc
luke7231 <tmddnjs7231@gmail.com>
2024-12-03 00:56:08 +0900

    feat: "지도에서도 내 위치 찾기 가능하다."

--- src/Routes/maps/index.tsx
+++ src/Routes/maps/index.tsx
@@ -5,6 +5,7 @@ import { DATA, IData } from "../../data";
 import { Product, Store } from "../../__generated__/graphql";
 import { StoreCard } from "../../components/store_card";
 import { useAuth } from "../../core/auth";
+import { postMessage } from "../../core/message";
 
 function getAddressY() {
   return localStorage.getItem("lat");
@@ -28,6 +29,8 @@ function Map({ stores }: { stores: Store[] }) {
   // 지도가 이동하였을 때 가려진 부분의 마커는 숨기고
   // 노출된 부분의 마커를 표시하는 작업을 하기 위해 map 인스턴스를 state로 관리
   const [newMap, setNewMap] = useState<naver.maps.Map | null>(null);
+  const [myLocationMarker, setMyLocationMarker] =
+    useState<naver.maps.Marker | null>(null);
 
   // [지도를 그리는 effect]
   // 중심이 될 위경도 값이 변경되면 지도를 새로 그려야하므로
@@ -61,6 +64,7 @@ function Map({ stores }: { stores: Store[] }) {
     setNewMap(map);
     //마커 생성 함수 호출
     addMarkers();
+    addMyLocationMarker();
     //검색 결과 거리순으로 재정렬하는 함수 호출
     resetListHandler();
     // }, [AddressX, AddressY, totalDomData, viewportWidth]);
@@ -103,6 +107,22 @@ function Map({ stores }: { stores: Store[] }) {
       //마커리스트에 추가
       createMarkerList.push(newMarker);
     } catch (e) {}
+  };
+
+  // [이전에 찍어놓은 내 위치 마커 생성]
+  const addMyLocationMarker = () => {
+    const Y = localStorage.getItem("my_location_lat");
+    const X = localStorage.getItem("my_location_lng");
+    if (Y && X) {
+      const position = new naver.maps.LatLng(Number(Y), Number(X));
+      const locationMarker = new naver.maps.Marker({
+        position,
+        map,
+        icon: "https://mahi-img.s3.ap-northeast-2.amazonaws.com/My_location.svg",
+      });
+      locationMarker.setTitle("my_baby");
+      setMyLocationMarker(locationMarker);
+    }
   };
 
   const [sortedData, setSortedData] = useState<IData[] | []>();
@@ -164,13 +184,46 @@ function Map({ stores }: { stores: Store[] }) {
   function onClickStore(id: number) {
     navigate(`/store/${id}`);
   }
-  const clickButton = () => {
-    // 원래 자리로 옮기는 것
-    // newMap?.panTo(new naver.maps.LatLng(37.3595704, 127.105399), {
-    //   duration: 0,
-    // });
-    resetListHandler();
-    console.log("내 현재 위치로 돌아오면서 재 정렬한다?");
+  const onClickMyLocation = () => {
+    postMessage("REQ_CURRENT_LOCATION", "");
+    const receiver = navigator.userAgent.includes("Android")
+      ? document
+      : window;
+    const listener = (event: any) => {
+      const appData = JSON.parse(event?.data);
+
+      if (appData?.type === "RES_CURRENT_LOCATION") {
+        const coords = appData.data.coords;
+        const latLng = new naver.maps.LatLng(coords.latitude, coords.longitude);
+        // 지도 이동
+        newMap?.panTo(latLng, {
+          duration: 0,
+        });
+
+        // 마커 [이동 or 생성]
+        if (myLocationMarker && myLocationMarker.getTitle() === "my_baby") {
+          // 이미 마커가 있다면 [이동]
+          myLocationMarker.setPosition(latLng); // 이게 되는가
+          setMyLocationMarker(myLocationMarker);
+        } else {
+          // 마커가 없다면 [생성]
+          const locationMarker = new naver.maps.Marker({
+            position: latLng,
+            map: newMap as any,
+            icon: "https://mahi-img.s3.ap-northeast-2.amazonaws.com/My_location.svg",
+          });
+          locationMarker.setTitle("my_baby");
+          setMyLocationMarker(locationMarker);
+        }
+
+        localStorage.setItem("my_location_lat", String(coords.latitude)); // 다음에 들어올 떄 쓰임
+        localStorage.setItem("my_location_lng", String(coords.longitude)); // 다음에 들어올 떄 쓰임
+      }
+
+      receiver.removeEventListener("message", listener);
+    };
+
+    receiver.addEventListener("message", listener);
   };
   const mapHeight = window.innerHeight - 45 - 56 - 64; // 1. 위치설정 바 2. 탭 3. 바텀탭
   return (
@@ -180,6 +233,29 @@ function Map({ stores }: { stores: Store[] }) {
         id="map"
         style={{ width: "100%", height: `${mapHeight}px` }}
       >
+        <div
+          className="bg-[#1562fc] rounded-md w-10 h-10 absolute bottom-6 right-6 z-50 text-center flex justify-center items-center text-white"
+          onClick={() => onClickMyLocation()}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <circle cx="12" cy="12" r="7"></circle>{" "}
+            <circle cx="12" cy="12" r="3" fill="currentColor"></circle>
+            <path d="M12 2v2"></path>
+            <path d="M12 20v2"></path>
+            <path d="M2 12h2"></path>
+            <path d="M20 12h2"></path>
+          </svg>
+        </div>
         {clickedStore ? (
           <div className="p-4 absolute bottom-1 z-50 w-full ">
             <div className="shadow-[0px_0px_144px_0px_rgba(21,98,252,0.40)]">
