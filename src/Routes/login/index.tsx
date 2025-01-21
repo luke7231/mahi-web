e2dc8b660b9ff2267c40bea6a2e531d9c1c1d3a4
luke7231 <tmddnjs7231@gmail.com>
2025-01-21 12:23:20 +0900

    feat: 로그인 페이지 디자인 변경

--- src/Routes/login/index.tsx
+++ src/Routes/login/index.tsx
@@ -140,10 +140,12 @@ const Login: React.FC = () => {
       console.log(error);
     }
   };
+  const [showEmailLogin, setShowEmailLogin] = useState(false);
+
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-[#fff] p-4">
-        <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
+        <div className="w-full bg-white rounded-lg p-2">
           <div onClick={() => navigate(-1)}>
             <BackArrow />
           </div>
@@ -153,94 +155,40 @@ const Login: React.FC = () => {
             <p className="text-sm text-[#757575]">계정에 로그인하세요</p>
           </div>
 
-          {/* Email and Password Fields */}
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-black mb-1">
-                이메일
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-4 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-[#1562fc]"
-                placeholder="이메일을 입력하세요"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-black mb-1">
-                비밀번호
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-4 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-[#1562fc]"
-                placeholder="비밀번호를 입력하세요"
-                required
-              />
-            </div>
-
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full py-2 bg-[#1562fc] text-white text-sm font-semibold rounded-md hover:bg-[#124ab7] transition duration-150"
-            >
-              로그인
-            </button>
-          </form>
-
-          {/* Divider */}
-          <div className="my-4 flex items-center">
-            <hr className="flex-grow border-t border-gray-300" />
-            <span className="mx-4 text-xs text-gray-400">또는</span>
-            <hr className="flex-grow border-t border-gray-300" />
-          </div>
-
           {/* Social Login Buttons */}
-          <div className="space-y-2">
+          <div className="space-y-3">
             {/* Sign Up Button */}
-            <div className="mt-4 text-center">
-              <button
-                className="w-full py-2 border border-[#1562fc] text-[#1562fc] text-sm font-semibold rounded-md hover:bg-[#e6f0ff] transition duration-150"
-                onClick={() => navigate("/sign-up")}
-              >
-                회원가입
-              </button>
-            </div>
+
             <button
               onClick={handleKakaoButtonClick}
-              className="w-full py-2.5 border border-[#fae300] bg-[#fae300] text-black text-sm font-semibold rounded-md flex items-center justify-center hover:bg-[#e8d800] transition duration-150"
+              className="w-full py-4 border border-[#fae300] bg-[#fae300] text-black text-lg font-semibold rounded-md flex items-center justify-center hover:bg-[#e8d800] transition duration-150"
             >
               <svg
-                width="16px"
-                height="16px"
+                width="20px"
+                height="18px"
                 viewBox="0 0 512 512"
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
-                className="mr-2"
+                className="mr-1"
               >
                 <path
                   fill="#000000"
                   d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
                 ></path>
               </svg>
-              카카오로 로그인
+              카카오로 계속하기
             </button>
 
             {isIOSApp() || isWeb() ? (
               <button
                 onClick={loginWithApple}
-                className="w-full py-2 bg-black text-white text-sm font-semibold rounded-md flex items-center justify-center hover:bg-gray-800 transition duration-150"
+                className="w-full py-3.5 bg-black text-white text-lg font-semibold rounded-md flex items-center justify-center hover:bg-gray-800 transition duration-150"
               >
                 <svg
                   version="1.0"
                   xmlns="http://www.w3.org/2000/svg"
                   width="28px"
-                  height="28px"
+                  height="32px"
                   viewBox="0 0 348.000000 348.000000"
                   preserveAspectRatio="xMidYMid meet"
                   className="pb-0.5"
@@ -264,13 +212,78 @@ const Login: React.FC = () => {
                     />
                   </g>
                 </svg>
-                Apple로 로그인
+                Apple로 계속하기
               </button>
             ) : null}
+            <div className="mt-4 text-center">
+              <button
+                className="w-full py-4 border bg-[#1692fc] border-[#1692fc] text-white font-bold text-lg rounded-md "
+                onClick={() => setShowEmailLogin(true)}
+              >
+                이메일로 로그인
+              </button>
+            </div>
+          </div>
+
+          {/* Email and Password Fields */}
+          {showEmailLogin && (
+            <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+              <div>
+                <label className="block text-sm font-medium text-black mb-1">
+                  이메일
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-[#1562fc]"
+                  placeholder="이메일을 입력하세요"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-black mb-1">
+                  비밀번호
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-[#1562fc]"
+                  placeholder="비밀번호를 입력하세요"
+                  required
+                />
+              </div>
+
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="w-full py-2 bg-[#1562fc] text-white text-sm font-semibold rounded-md hover:bg-[#124ab7] transition duration-150"
+              >
+                로그인
+              </button>
+            </form>
+          )}
+
+          {/* Divider */}
+          <div className="my-4 flex items-center">
+            <hr className="flex-grow border-t border-gray-300" />
+            <span className="mx-4 text-xs text-gray-400">또는</span>
+            <hr className="flex-grow border-t border-gray-300" />
           </div>
 
           {/* Additional Links */}
-          <div className="mt-6 text-center">
+          <div className="mt-4 text-center">
+            <div
+              onClick={() => navigate("/sign-up")}
+              className="text-sm underline text-[#757575] hover:text-[#1562fc]"
+            >
+              이메일로 가입하기
+            </div>
+          </div>
+          <div className="mt-4 text-center">
             <div
               onClick={() => navigate("/password-reset")}
               className="text-sm underline text-[#757575] hover:text-[#1562fc]"
